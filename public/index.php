b66352cb1db45049e5694d467870ce00ce04f04d
WISSAL KERKOUR <wsskekrour@gmail.com>
2023-10-26 18:51:26 +0200

    ok

---
+++ public/index.php
@@ -0,0 +1,6 @@
+<?php
+
+require "../vendor/autoload.php";
+
+$hello = new App\Hello();
+echo $hello->talk();
---
+++ src/Hello.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+class Hello 
+{
+    public function talk(): string
+    {
+        return "Hello World";
+    }
+}
